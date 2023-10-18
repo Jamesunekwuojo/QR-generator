71b37884abfc24dc493747139df22d68cc4fd3ed
James Unekwuojo <121576934+Jamesunekwuojo@users.noreply.github.com>
2023-10-18 18:21:13 +0100

    error handling added

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let QRimage = document.getElementById('QRimg')
 let QRtext = document.getElementById('QRtext')
 
 function QRgenerator() {
-    QRimage.src = "http://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.value;
-    QRbox.classList.add("display-img")
+    if(QRtext.value.length > 0){
+        QRimage.src = "http://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + QRtext.value;
+        QRbox.classList.add("display-img")
+
+    }else{
+        QRtext.classList.add('error');
+        setTimeout(()=>{
+            QRtext.classList.remove('error')
+        }, 1000)
+    }
+    
     
 }
